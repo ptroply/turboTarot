6649812fdeaec340c26d13c136c737f0fb9294b9
Matt Pietropaoli <87542182+ptroply@users.noreply.github.com>
2021-11-15 09:30:53 -0500

    Initial commit / uploaded project files

---
+++ turboTarot/TurboTarot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TurboTarot.Class;
+using TurboTarot.Service;
+
+namespace TurboTarot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TableService tableService = new TableService();
+            new ConsoleService(tableService);
+        }
+    }
+}
